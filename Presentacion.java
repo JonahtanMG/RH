efdcf0d02423e8086095f8b92a26cedcc923ddcc
Jonahtan <jmamanigut@unsa.edu.pe>
2025-07-21 22:44:22 -0500

    presentacion java

---
+++ Presentacion.java
@@ -0,0 +1,10 @@
+
+package presentacion;
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Soy Jonahtan Joaquín Mamani Gutierrez, tengo 17 años, estudio ingeniería de sistemas en la UNSA.\n" 
+        +"Elegí la carrera porque me gusta la lógica y los números.\n" 
+        +"Me gusta jugar videojuegos y escuchar música.");
+    }
+    
+}
